f0f45c6311c30aa4db7fe70c1dab68c57dd28b92
andy <andrei.liubinski@stefanini.com>
2024-07-01 18:00:27 +0300

    7/1/24 Objects done, Data types 50% done, started advanced functions

--- 3.Objects the basics/2.check-for-emptiness.js
+++ 3.Objects the basics/2.check-for-emptiness.js
@@ -10,16 +10,14 @@
 
 // alert( isEmpty(schedule) ); // false
 
-
-
 let newObject = new Object()
 
-const isEmpty = function(obj) {
-    let number_of_keys = 0
-    for (const key in obj) {
-        number_of_keys++
-    }
-    return number_of_keys>0 ? true : false
+const isEmpty = function (obj) {
+  let number_of_keys = 0
+  for (const key in obj) {
+    number_of_keys++
+  }
+  return number_of_keys === 0 ? true : false
 }
 
 newObject.someKindaKey = `Here be dragons`
---
+++ 4.Data types/13.Is array copied.js
@@ -0,0 +1,19 @@
+// What is this code going to show?
+
+// let fruits = ["Apples", "Pear", "Orange"];
+
+// // push a new value into the "copy"
+// let shoppingCart = fruits;
+// shoppingCart.push("Banana");
+
+// // what's in fruits?
+// alert( fruits.length ); // ?
+
+let fruits = ['Apples', 'Pear', 'Orange']
+
+// push a new value into the "copy"
+let shoppingCart = fruits
+shoppingCart.push('Banana')
+
+// what's in fruits?
+console.log(fruits.length) // IT WILL SHOW 4
